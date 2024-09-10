2712ffd9f119ce8ec2893c9ae83e451a87383746
Jacob Archambault <jacobarchambault@unknown00051b93e721.attlocal.net>
2024-09-10 13:33:31 -0400

    Add remove docker script

--- docker_init.sh
+++ docker_init.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2 docker-ce
+sudo yum install -y yum-utils docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl enable --now docker
 sudo usermod -aG docker cloud_user
